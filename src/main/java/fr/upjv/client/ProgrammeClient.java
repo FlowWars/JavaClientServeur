a15400d089318de022f6e23bdcedc3bbd1d7dd38
Macrelle_T <65347962+FlowWars@users.noreply.github.com>
2021-11-26 00:04:21 +0100

    Ajout de l'échange client-serveur

--- src/main/java/fr/upjv/client/ProgrammeClient.java
+++ src/main/java/fr/upjv/client/ProgrammeClient.java
@@ -1,26 +1,43 @@
 package fr.upjv.client;
 
-import java.io.BufferedOutputStream;
-import java.io.DataOutput;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ProgrammeClient {
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args){
 
         String ipServeur ="127.0.0.1";
         int numeroPort = 1234;
 
-        Socket socket=new Socket(ipServeur, numeroPort);
+        Socket socket=null;
+        DataInputStream dataInputStream=null;
+        DataOutputStream dataOutputStream=null;
 
         try {
+            socket=new Socket(ipServeur, numeroPort);
+            System.out.print("Nom client : ");
+            Scanner scanner = new Scanner(System.in);
+            String nomClient=scanner.next();
+            //On envoie la donnée au serveur
+            dataOutputStream=new DataOutputStream(socket.getOutputStream());
+            dataOutputStream.writeUTF(nomClient);
+            //on récupère la donnée envoyéé par le serveur
+            dataInputStream=new DataInputStream(socket.getInputStream());
+            String s1= dataInputStream.readUTF();
+            System.out.println(s1);
+
             socket.close();
+            scanner.close();
+            dataInputStream.close();
+            dataOutputStream.close();
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(-1);
         }
+
+
 
 
     }
--- src/main/java/fr/upjv/serveur/ProgrammeServeur.java
+++ src/main/java/fr/upjv/serveur/ProgrammeServeur.java
@@ -9,22 +9,25 @@ public class ProgrammeServeur {
     public static void main(String[] args) throws IOException{
 
         int numeroPort=1234;
-
         ServerSocket serverSocket = new ServerSocket(numeroPort);
-
-        Socket maSocket=null;
-
-        try {
-            maSocket = serverSocket.accept();
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.exit(-1);
-        }
-
-        System.out.println("client connecté");
+        System.out.println("En attente de connexion d'un client");
+        Socket socket=null;
 
         try {
-            serverSocket.close();
+            socket = serverSocket.accept();
+            System.out.println("Connexion réussie");
+            //On récupère la donnée envoyée par le client
+            DataInputStream dataInputStream=new DataInputStream(socket.getInputStream());
+            String nomClient = dataInputStream.readUTF();
+            //On effectue un traitement
+            String s1="Bienvenue "+nomClient+", vous etes bien connecté";
+            //On envoie la donnée au client
+            DataOutputStream dataOutputStream=new DataOutputStream(socket.getOutputStream());
+            dataOutputStream.writeUTF(s1);
+
+            socket.close();
+            dataInputStream.close();
+            dataOutputStream.close();
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(-1);
